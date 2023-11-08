ba27f9d3d21c4f17ba0d3b3bc40071dec4186aa7
archanarackenchath <archanarackenchath@gmail.com>
2023-11-08 22:36:59 +0530

    cart error corrected

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 let searchForm = document.querySelector('.search-form');
 
